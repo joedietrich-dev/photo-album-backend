388dba7f892b0154dafab5564081ad8cd6e4c418
joedietrich-dev <joe@joedietrich.net>
2021-10-15 15:26:28 -0400

    Set up basic backend json server

---
+++ server.js
@@ -0,0 +1,18 @@
+const jsonServer = require("json-server");
+const path = require("path");
+const cors = require("cors");
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db", "db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log("JSON Server is running");
+});
